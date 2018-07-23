e2e2cbd2d022cdc0de16bc1e46d3d0e65cc4d638
mtouray@vassar.edu <mtouray@sp309-189.cs.vassar.edu>
2018-07-23 17:01:31 -0400

    Prelim debugging, still need send monies contract--> address

--- emerfund.sol
+++ emerfund.sol
@@ -1,20 +1,9 @@
+
 pragma solidity ^0.4.4;
 
-contract ERC20Interface {
-    
-    function totalSupply() public constant returns (uint256 totalSupply);
-    function balanceOf(address owner) public returns (uint256 balance);
-    function allowance(address owner, address spender) public constant returns (uint256 remaining);
-    function transfer(address to, uint256 amount) public returns (bool success);
-    function approve(address spender, uint256 amount) public returns (bool success);
-    function transferFrom(address from, address to, uint256 amount) public returns (bool success);
-    
-    event Transfer(address indexed from, address indexed to, uint256 amount);
-    event Approval(address indexed owner, address indexed spender, uint256 amount);
-} 
  /*STILL NEED: 
-Proportional allocation FUNC 
-EVENT (use blocktime)// //Close Vote, calculate votes, show results, reset everything to 0 
+Send funds from contract to Admin
+EVENT //Close Vote,  show results, 
 Voter verification specific to Vassar (in presentation)
 Storing past votes from previous voting periods PYTHON TERM 
 */ 
@@ -25,8 +14,11 @@ contract EmerFund {
         uint id;
         string name;
         uint voteCount;
+        uint monies;
     }  
-
+    //list of voters 
+    address[] public votersList; 
+    
     //Store accounts that have voted bool= true means they voted 
     mapping(address => bool) public voters;
 
@@ -37,38 +29,39 @@ contract EmerFund {
      uint public fundsCount; 
 
     // monies variable 
-    uint monies 
+    uint monies;
 
     //Store monies (display total val??) 
     function TotMonies(uint amount) payable {
-        monies += amount
+        monies += amount;
     }
+    
 
     //Allocating 50 percent Equally
-    function equiAll {
+    function equiAll () private {
     //half monies divided by fundsCount
-        halfmoniesDFC = uint((monies / 2) / fundsCount) 
+        halfmoniesDFC = uint((monies / 2) / fundsCount);
     //do equal allocation to funds 
-        for (uint i=0; i<fundsCount; i++) 
-        {funds[i] += halfmoniesDFC}
+        for (uint i=0; i<fundsCount; i++){
+        funds[i].monies += halfmoniesDFC;
+        }
     }
 
     //total votes variable
-    uint TotVote 
+    uint TotVote;
 
     //final allocation of other 50 percent 
-    function FinalAll {
-        halfmonies = uint(monies / 2)
-    //get total votes
-        for (uint i=0; i<fundsCount; i++)
-        funds[i].voteCount + TotVote;
+    function FinalAll () public {
+        require (msg.sender == Admin);
+        halfmonies = uint(monies / 2);
     //get proportion of each fund's votes amongst TotVote 
-        for (uint i=0; i<fundsCount; i++)
-        proVote = funds[i].voteCount // TotVote
+        for (uint i=0; i<fundsCount; i++) {
+        proVote = (funds[i].voteCount / TotVote);
     //allocate monies proportionally to funds (proportion x total monies)
-        for (uint i=0; i<fundsCount; i++)
-        proMonies = (proVote x halfmonies) 
-        proMonies + funds[i]
+        proMonies = (proVote * halfmonies); 
+        funds[i].monies +=  proMonies;
+        }
+        reset; 
     }
 
     // voted event 
@@ -77,24 +70,36 @@ contract EmerFund {
     );
     
     //Close Vote: shows total votes, votes for each fund 
-    // how much monies each fund recieved, calls reset func 
-    event closeVote (
-        reset
-    )
+    // how much monies each fund recieved, 
+    // event closeVote (
+        // emit TotVote
+       // for (uint i=0; i<fundsCount; i++)
+        //emit funds[i].voteCount 
+        //emit //monies in fund 
+        //reset //NEED CORIN HLP 
+   // );
 
     //resets monie storage, total votes, votes for ind funds
-    function reset {
-    monies = 0
-    TotVote = 0
-    for (uint i=0; i<fundsCount; i++)
-        funds[i].voteCount = 0 
+    function reset () private {
+    monies = 0;
+    TotVote = 0;
+    for (uint i=0; i<votersList.length; i++){
+        voters[votersList[i]] = false;
+    }
+    for (uint i=0; i<fundsCount; i++){
+        funds[i].voteCount = 0 ;
+    }
+    }
 
 
     //Variable (chairperson) 
-    var address Admin 
+    address Admin;
+    
+    //Transfer Monies to Admin <-----
+    
 
     //Constructor 
-    function EmerFund () public {
+    constructor EmerFund () public {
         addFund("Physical Health");
         addFund("Mental Health");
         addFund("Financial Aid");
@@ -103,21 +108,21 @@ contract EmerFund {
         addFund("Travel");
         addFund("Food");
         addFund("Childcare"); 
-        Admin = msg.sender 
+        Admin = msg.sender;
     }
 
     //Add Fund
     function addFund (string _name) public {
-        require (msg.sender == Admin)
+        require (msg.sender == Admin);
         //increment the num of fund categories 
         fundsCount ++;
         funds[fundsCount] = Fund(fundsCount, _name, 0);
     }
 
-    //Remove Fundblockchain timestamp
+    //Remove Fund
     function remFund (string _name) public {
-        require (msg.sender == Admin)
-        require (_fundId > 0 && _fundId <= fundsCount)
+        require (msg.sender == Admin);
+        require (_fundId > 0 && _fundId <= fundsCount);
         //decrement the num of fund categories 
         fundsCount --;
         funds[fundsCount] = Fund(fundsCount, _name, 0); //clarify w corin 
@@ -125,30 +130,29 @@ contract EmerFund {
 
     //vote function
     function vote (uint  _fundId) public {
-    if TotVote = 0;
-        action_time = now
-
-    //require that they havent voted before (in a week), so msg sender is not in mapping//
-    require(!voters[msg.sender] && now = (action_time + 1 week)) 
+    //require that they havent voted before (in a week), so msg sender is not in mapping
+    require(!voters[msg.sender]);
     
     /*require a valid fund (the fund id is greater than 0 and less than the total amount of funds) 
     returns true >> continues executing  vote function 
     */
-    require (_fundId > 0 && _fundId <= fundsCount)
+    require (_fundId > 0 && _fundId <= fundsCount);
+    
+    // voterlist 
+    votersList.push(msg.sender);
     
     //record voters that have already voted 
-    voters[msg.sender] = true 
+    voters[msg.sender] = true;
 
     //Update Fund votecount by 1 
     funds[_fundId].voteCount ++;
+    TotVote ++;
 
     // trigger voted event 
     votedEvent(_fundId);
    
    //trigger close vote event
-   closeVote }
+   //closeVote }
 
-
-
-   
+}
 }
